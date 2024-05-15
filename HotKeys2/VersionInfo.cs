37e8e7e5bdff8d15c855ea1b40c02b64b2821119
jsakamoto <junichi.jp@gmail.com>
2024-05-15 10:17:34 +0900

    v.5.0.0-preview.6 release

--- HotKeys2/VersionInfo.cs
+++ HotKeys2/VersionInfo.cs
@@ -1,5 +1,5 @@
 namespace Toolbelt.Blazor.HotKeys2;
 internal static class VersionInfo
 {
-    internal const string VersionText = "5.0.0-preview.5";
+    internal const string VersionText = "5.0.0-preview.6";
 }
