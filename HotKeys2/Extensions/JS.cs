d5d00759baaf32053db0a5aec0dfe181f004523d
jsakamoto <junichi.jp@gmail.com>
2024-07-03 07:30:12 +0900

    Show the developers the guide message when using the Toolbelt.Blazor.getProperty JavaScript module is not loaded.

--- HotKeys2/Extensions/JS.cs
+++ HotKeys2/Extensions/JS.cs
@@ -7,6 +7,8 @@ namespace Toolbelt.Blazor.HotKeys2.Extensions;
 [SuppressMessage("Usage", "CA2254:Template should be a static expression")]
 internal static class JS
 {
+    private static bool ShowDeveloperGuide() => true;
+
     public static async ValueTask<IJSObjectReference> ImportScriptAsync(this IJSRuntime jsRuntime, ILogger logger)
     {
         var scriptPath = "./_content/Toolbelt.Blazor.HotKeys2/script.min.js";
@@ -15,7 +17,19 @@ internal static class JS
             var isOnLine = await jsRuntime.InvokeAsync<bool>("Toolbelt.Blazor.getProperty", "navigator.onLine");
             if (isOnLine) scriptPath += "?v=" + VersionInfo.VersionText;
         }
-        catch (JSException e) { logger.LogError(e, e.Message); }
+        catch (JSException e)
+        {
+            if (ShowDeveloperGuide())
+            {
+                if (e.Message.StartsWith("Could not find 'Toolbelt.Blazor.getProperty'"))
+                {
+                    logger.LogWarning(e.Message);
+                    logger.LogWarning("Please see also: https://github.com/jsakamoto/Toolbelt.Blazor.HotKeys2/issues/17#issuecomment-2195830001");
+                }
+                else logger.LogError(e, e.Message);
+            }
+            else logger.LogError(e, e.Message);
+        }
 
         return await jsRuntime.InvokeAsync<IJSObjectReference>("import", scriptPath);
     }
