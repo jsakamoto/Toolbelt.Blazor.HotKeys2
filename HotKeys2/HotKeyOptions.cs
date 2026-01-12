7dda61e46f6403d73b0c0fa33fbc421f24aeef65
jsakamoto <junichi.jp@gmail.com>
2026-01-12 21:55:29 +0900

    Deprecate HotKeyOptions.State property
    - Expose Disabled property directly for simpler syntax

--- HotKeys2/HotKeyOptions.cs
+++ HotKeys2/HotKeyOptions.cs
@@ -1,4 +1,6 @@
-﻿namespace Toolbelt.Blazor.HotKeys2;
+using System.ComponentModel;
+
+namespace Toolbelt.Blazor.HotKeys2;
 
 /// <summary>
 /// The options for a hotkey entry.
@@ -16,6 +18,18 @@ public class HotKeyOptions
     /// <summary>Additional CSS selector for HTML elements that will not allow hotkey to work.</summary>
     public string ExcludeSelector { get; set; } = "";
 
+    /// <summary>
+    /// Controls if the current hot key is disabled or not.
+    /// </summary>
+    public bool Disabled
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        get => this.State.Disabled;
+        set => this.State.Disabled = value;
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
     /// <summary>State data attached to a hotkey.</summary>
+    [Obsolete("Use the 'Disabled' property of HotKeyOptions directly instead."), EditorBrowsable(EditorBrowsableState.Never)]
     public HotKeyEntryState State { get; set; } = new HotKeyEntryState();
 }
